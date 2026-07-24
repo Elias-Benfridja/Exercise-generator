7b6047297fd0436db12aaf38c04390d4e2043189
Elias <Elias.Benfridja06@gmail.com>
2026-07-25 00:52:07 +0100

    host issue

--- backend/config/settings/deployment.py
+++ backend/config/settings/deployment.py
@@ -3,7 +3,7 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://exercise-generator-qq0h.onrender.com']
+ALLOWED_HOSTS = ['exercise-generator-qq0h.onrender.com', '.onrender.com']
 
 DATABASES = {
     'default': {
